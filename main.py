97e7304be4f2cd0dbe49934f4391fd244386ee30
saadat <saadat2033@gmail.com>
2026-09-15 01:55:40 +0530

    Add basic CRUD logic for expenses (in-memory)

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print("hello")
+expenses=[]
+def add_expense(amount,category,description):
+ expense={"amount":amount,"category":category,"description":description}
+ expenses.append(expense)
+ print(expenses)
+
+add_expense(12, "Food", "lunch")
+add_expense(8, "Transport", "bus")
+
+def view_expense():
+ for expense in expenses:
+   print(expense)
+
+def delete_expense(amount, category, description):
+    expense = {"amount": amount, "category": category, "description": description}
+    if expense in expenses:
+        expenses.remove(expense)
+        print(expense, "removed")
+    else:
+        print(expense, "NOT FOUND")
+ 
+ 
+delete_expense(999,"Nonsense","fake")
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+999,"Nonsense","fake"
